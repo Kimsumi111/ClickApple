e08d85bac5b65d9bb822444229ede50b1280956a
Kimsumi111 <rlatnal234@naver.com>
2024-05-07 15:31:29 +0900

    Feat : Gameover implement

--- Assets/2_Scripts/GameManager.cs
+++ Assets/2_Scripts/GameManager.cs
@@ -10,8 +10,12 @@ public class GameManager : MonoBehaviour
     [SerializeField] private int maxScore;
     // 생성 조건 : 일정 조건 이상의 스코어
     [SerializeField] private int noteGroupSpawnCondition = 10;
+    [SerializeField] private GameObject gameClearObj = null;
+    [SerializeField] private GameObject gameOverObj = null;
     private int nextNoteGroupUnlockCnt;
     private int score;
+
+    [SerializeField] private float maxTime = 30f;
 
     void Awake()
     {
@@ -20,7 +24,31 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         UIManager.Instance.OnScore(this.score, this.maxScore);
+
         NoteManager.Instance.Activate();
+
+        this.gameClearObj.SetActive(false);
+        this.gameOverObj.SetActive(false);
+
+        StartCoroutine(this.OnTimer());
+
+    }
+    IEnumerator OnTimer()
+    {
+        float _currentTime = 0f;
+
+        while (this.maxTime > _currentTime)
+        {
+            _currentTime += Time.deltaTime;
+
+            UIManager.Instance.OnTimer(_currentTime, this.maxTime);
+
+            yield return null;
+        }
+
+        // GameOver
+
+        this.gameOverObj.SetActive(true);
     }
 
     public void OnScore(bool _isCorrect)
@@ -36,10 +64,19 @@ public class GameManager : MonoBehaviour
                 // 노트 생성
                 NoteManager.Instance.OnSpawnNoteGroup();
             }
+            if(this.maxScore <= this.score)
+            {
+                // Game Clear
+                this.gameClearObj.SetActive(true);
+            }
         }
         else
             this.score--;
 
         UIManager.Instance.OnScore(this.score, this.maxScore);
+    }
+    public void CallBtn_Restart()
+    {
+
     }
 }
--- Assets/2_Scripts/UIManager.cs
+++ Assets/2_Scripts/UIManager.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     public static UIManager Instance;
+
     [SerializeField] private Image scoreImg = null;
     [SerializeField] private TMPro.TextMeshProUGUI scoreTmp = null;
+
+    [SerializeField] private Image timerImg = null;
+    [SerializeField] private TMPro.TextMeshProUGUI timerTmp = null;
 
     private void Awake()
     {
@@ -19,5 +24,11 @@ public class UIManager : MonoBehaviour
         this.scoreTmp.text = $"{_currentScore}/{_maxScore}";
 
         this.scoreImg.fillAmount = (float)_currentScore / _maxScore;
+    }
+    public void OnTimer(float _currentTimer, float _maxTimer)
+    {
+        this.timerTmp.text = $"{_currentTimer.ToStringN(1)}/{_maxTimer.ToStringN(1)}";
+
+        this.timerImg.fillAmount = (float)_currentTimer / _maxTimer;
     }
 }
